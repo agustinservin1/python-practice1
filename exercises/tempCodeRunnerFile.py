df299ccbac08f8ccfa88424cd5eaefdb2aa20175
Agustín Servin <agustinservin199103@gmail.com>
2024-04-07 14:26:40 -0300

    Resolución hasta ejercicio 8 inclusive - Práctica 1

---
+++ exercises/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+Compras = {
+"Mario Pedernera": ["cafetera", "TV 50 pulgadas", "mouse gamer"],
+"Ezequiel Castello": ["ipad", "ipod", "iphone"],
+"Pablo Piristrelli": ["Reproductor de CD", "Videograbadora"]
+}
+# COMPLETAR - FIN
+
+assert (
+    (Compras["Mario Pedernera"] == ["cafetera", "TV 50 pulgads", "mouse gamer"])
+    and (Compras["Ezequiel Castello"] == ["ipad", "ipod", "iphone"])
+    and (Compras["Pablo Piristrelli"] == ["Reproductor de CD", "Videograbadora"])
+)
+
